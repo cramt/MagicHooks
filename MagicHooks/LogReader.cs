30238399c8e0b3613f958837c3d66bb9638ca842
cramt <mads9220@gmail.com>
2020-01-23 01:59:17 +0100

    a

--- MagicHooks/LogReader.cs
+++ MagicHooks/LogReader.cs
@@ -7,29 +7,20 @@ using System.Threading.Tasks;
 namespace MagicHooks {
     public class LogReader {
         private string path;
-        private Encoding encoding = Encoding.UTF8;
         private StringBuilder data = new StringBuilder();
+        private EventHandler<string> onLogLine;
         public LogReader(string path) {
             this.path = path;
             StreamReader stream = new StreamReader(File.Open(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite));
             Task.Factory.StartNew(() => {
                 while (true) {
                     string str = stream.ReadLine();
-                    if(str == null) {
+                    if (str == null) {
                         continue;
                     }
-                    Console.WriteLine(str);
-                    data.Append(str);
+                    onLogLine?.Invoke(this, str);
                 }
             });
-        }
-        public void Read() {
-            
-        }
-        public string Data {
-            get {
-                return data.ToString();
-            }
         }
     }
 }
